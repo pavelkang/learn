acbac371d5558c720b008ef9318c9a38c3ba8277
Kai Kang <sclingtt@gmail.com>
2020-09-30 12:31:47 -0700

    remove files

---
+++ data_structure/priority_queue.py
@@ -0,0 +1,15 @@
+import unittest
+
+class MinHeap:
+
+    def __init__(self) -> None:
+        pass
+
+    def push(self, element):
+        pass
+
+    def pop(self):
+        pass
+
+    def peek(self):
+        pass
